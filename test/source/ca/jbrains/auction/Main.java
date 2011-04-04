e8fffa5fa2523fc3d09e13ff86fea34eaf11e479
J. B. Rainsberger <me@jbrains.ca>
2011-04-04 12:59:05 -0400

    Implemented an empty MainWindow for the user interface. Continue at the top of page 97.

--- test/source/ca/jbrains/auction/Main.java
+++ test/source/ca/jbrains/auction/Main.java
@@ -1,10 +1,36 @@
 package ca.jbrains.auction;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class Main {
+	public static class MainWindow extends JFrame {
+		public MainWindow() {
+			super("Auction Sniper");
+			setName(MAIN_WINDOW_NAME);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+		}
+	}
+
 	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
 	public static final String SNIPER_STATUS_NAME = "sniper status name";
+	private MainWindow ui;
 
-	public static void main(String xmppHostname, String sniperId,
-			String sniperPassword, String itemId) {
+	public Main() throws Exception {
+		startUserInterface();
+	}
+
+	private void startUserInterface() throws Exception {
+		SwingUtilities.invokeAndWait(new Runnable() {
+			@Override
+			public void run() {
+				ui = new MainWindow();
+			}
+		});
+	}
+
+	public static void main(String... args) throws Exception {
+		Main main = new Main();
 	}
 }
