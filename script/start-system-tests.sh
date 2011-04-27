3adcfe64f02ff491e0144e4bdd751c7eea67f04b
J. B. Rainsberger <me@jbrains.ca>
2011-04-26 23:24:22 -0400

    Tried writing start/stop scripts, but they don't seem to work. Shitty.

--- script/start-system-tests.sh
+++ script/start-system-tests.sh
@@ -1,4 +1,3 @@
-ejabberdctl --config ./system-test/config/ejabberd/learning-tests.cfg start
+ejabberdctl --config system-test/config/ejabberd/learning-tests.cfg start
 ejabberdctl register sniper localhost sniper
 ejabberdctl register auction-item-54321 localhost auction
-
---
+++ script/stop-system-tests.sh
@@ -0,0 +1,2 @@
+ejabberdctl stop
+
