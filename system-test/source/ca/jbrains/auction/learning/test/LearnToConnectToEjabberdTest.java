f237e8482d6210b978557d54e915d29cdf44228e
J. B. Rainsberger <me@jbrains.ca>
2011-04-26 21:34:41 -0400

    I think I've figured out how to authenticate using SASL with Smack and ejabberd: put PLAIN in front of DIGEST-MD5 in the list of authentications.

--- system-test/source/ca/jbrains/auction/learning/test/LearnToConnectToEjabberdTest.java
+++ system-test/source/ca/jbrains/auction/learning/test/LearnToConnectToEjabberdTest.java
@@ -19,6 +19,10 @@ public class LearnToConnectToEjabberdTest {
 		SmackConfiguration.setPacketReplyTimeout(60000);
 	}
 
+	public void pause() throws Exception {
+		Thread.sleep(10000L);
+	}
+
 	@Test
 	public void connectThenDisconnect() throws Exception {
 		final ConnectionListener connectionListener = mockery
@@ -37,10 +41,67 @@ public class LearnToConnectToEjabberdTest {
 	}
 
 	@Test
-	public void login() throws Exception {
-		XMPPConnection xmppConnection = new XMPPConnection("localhost");
+	public void authenticateWithSasl() throws Exception {
+		final String username = "sniper";
+		final String password = "sniper";
+		final String serviceName = "localhost";
+		
+		final ConnectionConfiguration config = new ConnectionConfiguration(
+				serviceName);
+		config.setCompressionEnabled(false);
+		config.setSASLAuthenticationEnabled(true);
+		config.setDebuggerEnabled(XMPPConnection.DEBUG_ENABLED);
+
+		final XMPPConnection xmppConnection = new XMPPConnection(config);
 		xmppConnection.connect();
-		xmppConnection.login("sniper", "sniper");
+		assertTrue(xmppConnection.isConnected());
+		assertFalse(xmppConnection.isAuthenticated());
+
+		assertTrue(config.isSASLAuthenticationEnabled());
+
+		final SASLAuthentication saslAuthentication = xmppConnection
+				.getSASLAuthentication();
+		assertTrue(saslAuthentication.hasNonAnonymousAuthentication());
+		assertNotNull(password);
+
+		SASLAuthentication.supportSASLMechanism("PLAIN", 0);
+		final String response = saslAuthentication.authenticate(username,
+				password, serviceName);
+		assertNotNull(response);
+
+		xmppConnection.disconnect();
+	}
+
+	@Test
+	public void authenticateWithoutSasl() throws Exception {
+		final String username = "sniper";
+		final String password = "sniper";
+		final String serviceName = "localhost";
+
+		final ConnectionConfiguration config = new ConnectionConfiguration(
+				serviceName);
+		config.setCompressionEnabled(false);
+		config.setSASLAuthenticationEnabled(false);
+		config.setDebuggerEnabled(XMPPConnection.DEBUG_ENABLED);
+
+		final XMPPConnection xmppConnection = new XMPPConnection(config);
+		xmppConnection.connect();
+		assertTrue(xmppConnection.isConnected());
+		assertFalse(xmppConnection.isAuthenticated());
+
+		final String response = new VisibleNonSASLAuthentication(xmppConnection)
+				.authenticate(username, password, serviceName);
+		assertNotNull(response);
+
+		xmppConnection.disconnect();
+	}
+
+	@Ignore("Waiting for the Saff Squeeze to help")
+	@Test
+	public void login() throws Exception {
+		final XMPPConnection xmppConnection = new XMPPConnection("localhost");
+		xmppConnection.connect();
+		xmppConnection.login("jbrains", "jbra1ns", "localhost");
 		xmppConnection.disconnect();
 	}
 }
---
+++ system-test/source/org/jivesoftware/smack/VisibleNonSASLAuthentication.java
@@ -0,0 +1,8 @@
+package org.jivesoftware.smack;
+
+// SMELL I need this because Smack made NonSASLAuthentication package-visible
+public class VisibleNonSASLAuthentication extends NonSASLAuthentication {
+	public VisibleNonSASLAuthentication(XMPPConnection connection) {
+		super(connection);
+	}
+}
