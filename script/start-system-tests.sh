bbf5fa80fcf3ee0ca5c0f3675cdfe381f4f97dee
J. B. Rainsberger <me@jbrains.ca>
2011-04-26 22:51:36 -0400

    Added users to the ejabberd server.

--- script/start-system-tests.sh
+++ script/start-system-tests.sh
@@ -1,2 +1,4 @@
 ejabberdctl --config ./system-test/config/ejabberd/learning-tests.cfg start
+ejabberdctl register sniper localhost sniper
+ejabberdctl register auction-item-54321 localhost auction
 
