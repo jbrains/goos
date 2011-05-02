b406d8860d065db53a28f81e64cdf2547bd3dbcb
J. B. Rainsberger <me@jbrains.ca>
2011-05-02 13:30:16 -0400

    Tried to test microtest for interpreting the "join auction" message, but couldn't. Or at least, didn't want to.

--- test/source/ca/jbrains/auction/message/test/InterpretChatMessageAsAuctionEventTest.java
+++ test/source/ca/jbrains/auction/message/test/InterpretChatMessageAsAuctionEventTest.java
@@ -2,7 +2,14 @@ package ca.jbrains.auction.message.test;
 
 import org.junit.*;
 
+import static org.junit.Assert.*;
+
 public class InterpretChatMessageAsAuctionEventTest {
+    @Test
+    public void joinAuctionMessage() throws Exception {
+        fail("Aha! We use empty message to mean both 'I joined an auction' and 'auction closed'. This can't last.");
+    }
+
     @Ignore("TODO")
     @Test
     public void reportPriceMessage() throws Exception {
